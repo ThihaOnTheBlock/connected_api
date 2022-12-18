99507dd3f51fe056ca8d7510b6c3ddf4acf6f822
ThihaOnTheBlock <thihaofficial123@gmail.com>
2022-12-18 17:41:01 -0500

    add fileupload middleware

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ import morgan from "morgan";
 
 import path from "path";
 
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 dotenv.config();
 
 const app = express();
@@ -23,6 +29,33 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use(helmet());
+
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+
+app.use(morgan("common"));
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
+
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({
+  storage,
+});
 
 app.get("/", (req, res) => {
   res.json({ message: "it is working" });
